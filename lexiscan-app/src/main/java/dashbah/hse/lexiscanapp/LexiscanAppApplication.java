659b9d9de675e59837b467a7ce26f8b7e41b4d65
dashbah <dsbakhankova@edu.hse.ru>
2025-02-01 22:37:53 +0300

    create maven project

---
+++ lexiscan-app/src/main/java/dashbah/hse/lexiscanapp/LexiscanAppApplication.java
@@ -0,0 +1,13 @@
+package dashbah.hse.lexiscanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexiscanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexiscanAppApplication.class, args);
+	}
+
+}
