7196de6bb12a38fe2480fe450c6bb5c477ff0e7d
dashbah <dsbakhankova@edu.hse.ru>
2025-05-03 14:12:32 +0300

    ml-model stub deleted

--- ml-model/src/main/java/dashbah/hse/lexiscan/ml/controller/ModelController.java
+++ ml-model/src/main/java/dashbah/hse/lexiscan/ml/controller/ModelController.java
@@ -1,8 +1,10 @@
 package dashbah.hse.lexiscan.ml.controller;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dashbah.hse.lexiscan.ml.dto.MlModelRq;
 import dashbah.hse.lexiscan.ml.dto.MlModelRs;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +13,19 @@ import java.util.Random;
 @RestController
 @RequestMapping("/ml-model")
 @RequiredArgsConstructor
+@Slf4j
 public class ModelController {
 
     private final Random rnd = new Random();
 
     @PostMapping("/count")
-    public ResponseEntity<MlModelRs> count(@RequestBody MlModelRq mlModelRq) {
-        return ResponseEntity.ok(MlModelRs.builder()
-                .rquid(mlModelRq.getRquid())
-                .percentage(rnd.nextDouble(0, 1))
+    public ResponseEntity<MlModelRs> count() {
+        var rs = ResponseEntity.ok(MlModelRs.builder()
+                .confidence(rnd.nextDouble(0, 1))
+                .prediction("Dyslexia")
                 .build());
+        log.info("response: " + rs);
+        return rs;
     }
 
 }
